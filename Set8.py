c9bb727c6d7d42ddf0cc943571253396b0bc7484
Hank <hpstennes@gmail.com>
2021-07-14 11:12:14 -0500

    stuff

--- Set8.py
+++ Set8.py
@@ -1,30 +1,7 @@
-import nt_helpers
+import nt_helpers as nt
 
-'''
-for x in range(105):
-    if x * x % 105 == 4: print(x)
-'''
-
-'''
-print(nt_helpers.list_powers_of(61, 2))
-print(nt_helpers.order(61, 2))
-print(nt_helpers.list_perfect_powers(61, 8))
-print(nt_helpers.phi(20))
-'''
-
-'''
-results = []
-for x in range(1, 21):
-    results.append((x, nt_helpers.phi(x)))
-'print(results)'
-'''
-
-'''
-for x in range(3, 101, 2):
-    order = nt_helpers.order(x, 2)
-    print(x, order)
-'''
-
-for x in nt_helpers.list_primes(100):
-    if not nt_helpers.is_generator(x, 2): print(x)
-
+print(nt.order(13, 2))
+print(nt.order(13, 4))
+print(nt.order(13, 8))
+print(nt.order(13, 16))
+print(nt.order(13, 3))
